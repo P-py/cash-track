cdf99e961efc9000b2c6c9466f7f4288a12e2972
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-15 01:44:49 -0300

    :dizzy: add dynamic functions for about page
    Co-authored-by: Ronaldo Simeone <ronaldosimeon3@gmail.com>

--- js/about.js
+++ js/about.js
@@ -1,12 +1,104 @@
 const API_URL = 'https://cashtrack-deploy-production.up.railway.app';
 //const API_URL = 'http://localhost:8080';
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.burger');
     const navLinks = document.querySelector('.nav-links');
+    const expandableImages = document.querySelectorAll('.expandableImage');
+    const modal = document.getElementById('imageModal');
+    const modalImg = document.getElementById('modalImage');
+    const modalContent = modal.querySelector('.modal-content');
+    const closeModal = modal.querySelector('.close');
+    const loginBtn = document.getElementById('login-btn');
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    const accessToken = getCookie('accessToken');
+    if(accessToken) {
+        loginBtn.textContent = 'Seu Dashboard';
+    }
+
+    expandableImages.forEach(image => {
+        image.addEventListener('click', () => {
+            modal.style.display = 'flex';
+            modalImg.src = image.src;
+            modalImg.alt = image.alt;
+
+            // Verifies if the image is the diagram image
+            // This image has a different height, so we need to adjust the margin-top
+            if (window.innerWidth > 768 && image.src.includes("data-diagram.png")) {
+                modalContent.style.marginTop = '55vh';
+            } else {
+                modalContent.style.marginTop = '0';
+            }
+        });
+    });
 
     // When the burger button is clicked, activates the hidden nav menu
     burger.addEventListener('click', () => {
+        console.log("menu clicado caralha")
         navLinks.classList.toggle('nav-active');
+    });
+
+    // Close the modal when the user clicks on the close button
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Close the modal when the user clicks outside the modal
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    // Swiper.js instance for the image content
+    const swiperImages = new Swiper('.what-we-do-images', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        lazyPreloadPrevNext: 0, // Does not render the next and previous slides for data saving
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // Responsive breakpoints
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+
+    // Swiper.js instance for the contact section
+    const swiperContact = new Swiper('.who-we-are-container-mobile', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        lazyPreloadPrevNext: 0, // Does not render the next and previous slides for data saving
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // Responsive breakpoints
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
     });
 });
